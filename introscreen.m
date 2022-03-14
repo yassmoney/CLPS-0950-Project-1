0875576aebbea6cac7b6c9c4494172e78c27eb1c
Yasemin Emami <yasemin_emami@brown.edu>
2022-03-14 15:47:50 -0400

    Intro Screen
    This code displays a title screen with a cute picture of our group and some text introducing us!

---
+++ introscreen.m
@@ -0,0 +1,16 @@
+%% Intro Page %%
+
+
+pic= imread("IMG_9145.jpeg","jpg")
+imshow(pic)
+
+position = [300 800]; 
+box_color = {'yellow'};
+text_str= ' This is the Stroop Groop :)'
+
+RGB = insertText(pic,position,text_str,'FontSize',50,'BoxColor',...
+    box_color,'BoxOpacity',.9,'TextColor','red');
+figure
+imshow(RGB)
+
+
---
+++ strooplevel1.m
@@ -0,0 +1,16 @@
+%% STROOP TEST LEVEL 1 %%
+
+%Colors Being Used
+
+% Tasks
+% 1. list of colors
+% 2. associated color values
+% 3. display 
+% 4. collect user input 
+% 5. store user input in an array for # of trials
+% 6. display results
+
+
+colors= ['red';'green';'blue';'yellow'];
+colorvalues= []
+
