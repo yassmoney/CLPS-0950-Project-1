c8b147ba1302cdb7be70799b14a573b7a277bfea
Yasemin Emami <yasemin_emami@brown.edu>
2022-03-17 16:28:49 -0400

    New Levels
    Created 4 levels of the Stroop test!

--- strooplevel1.m
+++ strooplevel1.m
@@ -10,58 +10,224 @@
 % 5. store user input in an array for # of trials
 % 6. display results
 
-%Set Colors for Level
-words= ['red';'green';'blue';'yellow']; % this is setting the words we will use for Level 1
-colorvalues= [255 0 0; 0 255 0; 0 0 255;255 255 0] % this sets the color values we are going to use
+% Clearing the workspace before start of the level
+close all;
+clear;
+sca;
 
-%Initialize Variables
-numberofcolors= 4;
-trials= 10;
-congruentpairs= [];
-congruentrts= zeros(trials,:) %check semicolon
-congruenterrors= [];
-incongruentpairs=[]; 
-incongruentrts= zeros(trials,:) %check semicolon
-incongruenterrors= [];
+% Setting up default values
+PsychDefaultSetup(2);
+
+% Create a random number generator
+rand('seed', sum(100 * clock));
+
+% Setting the screen number to the external secondary monitor if there is one
+% connected
+screenNumber = max(Screen('Screens'));
+
+% Define black, white and grey
+white = WhiteIndex(screenNumber);
+grey = white / .5; % making a lighter background
+black = BlackIndex(screenNumber);
+
+% Open the screen
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey, [], 32, 2);
+
+% Flip to clear
+Screen('Flip', window);
+
+% Query the frame duration
+ifi = Screen('GetFlipInterval', window);
+
+% Set the text size
+Screen('TextSize', window, 70);
+
+% Query the maximum priority level
+topPriorityLevel = MaxPriority(window);
+
+% Get the centre coordinate of the window
+[xCenter, yCenter] = RectCenter(windowRect);
+
+% Set the blend funciton for the screen
+Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
 
-%Running the Stroop Test Trials for congruent trials
+%----------------------------------------------------------------------
+%                       Timing Information
+%----------------------------------------------------------------------
+
+% Interstimulus interval time in seconds and frames
+isiTimeSecs = 1;
+isiTimeFrames = round(isiTimeSecs / ifi);
+
+% Number of frames to wait before re-drawing
+waitframes = 1;
 
 
-for ii= 1:trials % setting this to run the indicated number of trials
-    display= randi(numberofcolors) % randomly choosing a number correlated to a color to display
-    displaytext= words(display)  %using the randomly chosen color to display the word
-    displaycolor=colors(display)%using the randomly chosen color to color the word
- tic; %starting a timer
- congruentpairs(ii,:)= clock; %creating an array for data of the congruent pairs
- waitforbuttonpress
- response= double(get(gcf,'CurrentCharacter'))
- time=toc;
+%----------------------------------------------------------------------
+%                       Keyboard Presses
+%----------------------------------------------------------------------
+
+% Define the keyboard keys that are listened for. We will be using the left
+% and right arrow keys as response keys for the task and the escape key as
+% a exit/reset key
+greenKey = KbName('g');
+redKey = KbName('r');
+yellowKey = KbName('y');
+blueKey = KbName('b');
+escapeKey= KbName('ESCAPE');
 
 
 
- if response == 114 && display==0
-     
+%----------------- -----------------------------------------------------
+%                     Colors in words and RGB
+%----------------------------------------------------------------------
 
- congruentdata = [congruentdata,time]
-  
-for ii = 1:trials %creating the stroop test for incongruent trials)
-    display = randi(numberofcolors); % randomly choosing a number correlated to a color to display
-    displaycolor = colors(display); %using the randomly chosen color to display the word
-    jj = display; 
- 
-    while jj == display
-        jj = randi(numberofcolors); %choosing color for the world
+% We are going to use four colors!
+wordList = {'RED', 'GREEN', 'BLUE', 'YELLOW'};
+rgbColors = [1 0 0; 0 1 0; 0 0 1; 1 1 0];
+
+% Make the matrix which will determine our condition combinations
+condMatrixBase = [sort(repmat([1 2 3 4], 1, 3)); repmat([1 2 3 4], 1, 3)];
+
+% Number of trials per condition. We set this to one to give
+% us a total of 16 trials.
+trialsPerCondition = 1;
+
+% Duplicate the condition matrix to get the full number of trials
+condMatrix = repmat(condMatrixBase, 1, trialsPerCondition);
+
+% Get the size of the matrix
+[~, numTrials] = size(condMatrix);
+
+% Randomise the conditions
+shuffler = Shuffle(1:numTrials);
+condMatrixShuffled = condMatrix(:, shuffler);
+
+
+%----------------------------------------------------------------------
+%                     Make a response matrix
+%----------------------------------------------------------------------
+
+% This is a four row matrix the first row will record the word we present,
+% the second row the color the word it written in, the third row the key
+% they respond with and the final row the time they took to make there response.
+respMat = nan(4, numTrials);
+
+
+%----------------------------------------------------------------------
+%                       Experimental loop
+%----------------------------------------------------------------------
+
+% Animation loop: we loop for the total number of trials
+for trial = 1:numTrials
+
+    % Word and color number
+    wordNum = condMatrixShuffled(1, trial);
+    colorNum = condMatrixShuffled(2, trial);
+
+    % The color word and the color it is drawn in
+    theWord = wordList(wordNum);
+    theColor = rgbColors(colorNum, :);
+
+    % Cue to determine whether a response has been made
+    respToBeMade = true;
+
+    % If this is the first trial we present a start screen and wait for a
+    % key-press
+    if trial == 1
+        DrawFormattedText(window, 'Welcome to the Stroop Test Level 1! \n\n Press any key to see instructions!',...
+            'center', 'center', black);
+        Screen('Flip', window);
+        KbStrokeWait;
+        DrawFormattedText(window, 'A word will appear on your screen \n\n and will be colored either red, yellow, green or blue.\n\n If green, press the g key! \n\n If red, press the r key! \n\n If yellow, press the y key!, \n\n If blue, press the b key! \n\n Press any key to start!! \n\n  You can quit anytime by pressing ESC!','center', 'center', black)
+        Screen('Flip', window);
+        KbStrokeWait;
     end
-     displaytext = colors(display); %using the randomly chosen color to display the word
-     tic; %starting a timer
-     incongruentpairs(ii,:)= clock;%creating an array for data of the incongruent pairs
-     response= getkey(); 
-     time=toc;
+
+    % Flip again to sync us to the vertical retrace at the same time as
+    % drawing our fixation point
+    Screen('DrawDots', window, [xCenter; yCenter], 10, black, [], 2);
+    vbl = Screen('Flip', window);
+
+    % Now we present the isi interval with fixation point minus one frame
+    % because we presented the fixation point once already when getting a
+    % time stamp
+    for frame = 1:isiTimeFrames - 1
+
+        % Draw the fixation point
+        Screen('DrawDots', window, [xCenter; yCenter], 10, black, [], 2);
+
+        % Flip to the screen
+        vbl = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+    end
+
+    % Now present the word in continuous loops until the person presses a
+    % key to respond. We take a time stamp before and after to calculate
+    % our reaction time. We could do this directly with the vbl time stamps,
+    % but for the purposes of this introductory demo we will use GetSecs.
+    %
+    % The person should be asked to respond to either the written word or
+    % the color the word is written in. They make thier response with the
+    % three arrow key. They should press "Left" for "Red", "Down" for
+    % "Green" and "Right" for "Blue".
+    tStart = GetSecs;
+    while respToBeMade == true
+
+        % Draw the word
+        DrawFormattedText(window, char(theWord), 'center', 'center', theColor);
+
+        % Check the keyboard. The person should press the
+        [keyIsDown,secs, keyCode] = KbCheck;
+        if keyCode(escapeKey)
+            ShowCursor;
+            sca;
+            return
+        elseif keyCode(redKey)
+            response = 1;
+            respToBeMade = false;
+        elseif keyCode(blueKey)
+            response = 2;
+            respToBeMade = false;
+        elseif keyCode(greenKey)
+            response = 3;
+            respToBeMade = false;
+        elseif keyCode(yellowKey)
+            response= 4;
+            respToBeMade = false;
+        end
+
+        % Flip to the screen
+        vbl = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+    end
+    tEnd = GetSecs;
+    rt = tEnd - tStart;
+
+    % Record the trial data into out data matrix
+    respMat(1, trial) = wordNum;
+    respMat(2, trial) = colorNum;
+    respMat(3, trial) = response;
+    respMat(4, trial) = rt;
+
+end
+
+% Result Data
 
 
 
 
-    
+% End of experiment screen. We clear the screen once they have made their
+% response
+DrawFormattedText(window, 'You have completed Level One! \n\n Press any key to see your results',...
+    'center', 'center', black);
+Screen('Flip', window);
+KbStrokeWait;
+DrawFormattedText(window,' Below you will find your accuracy \n\n and your average reaction time!','center', 'center', black);
+Screen('Flip', window);
+KbStrokeWait;
 
+% Clearing the workspace before start of level
+close all;
+clear;
+sca;
 
