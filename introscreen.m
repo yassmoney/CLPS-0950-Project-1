6a09f11a44fb644e5a436b45e741693e630bd5b0
Yasemin Emami <yasemin_emami@brown.edu>
2022-04-27 10:48:40 -0400

    Menu + Working Memory
    Working to integrate the midterm with our final by creating menu screen + starting playing around with psychtoolbox demos to see which can be used for the working memory task

--- introscreen.m
+++ introscreen.m
@@ -15,30 +15,97 @@ Stroop = insertText(pic,position,text_str,'FontSize',50,'BoxColor',...
     box_color,'BoxOpacity',.9,'TextColor','red');
 figure
 imshow(Stroop)
+KbStrokeWait
+
+%% MENU SCREEN %%
+
+% Clearing the workspace 
+close all;
+clear;
+sca;
+%----------------------------------------------------------------------
+%                       Defining Information
+%----------------------------------------------------------------------
+% Setting up default values
+PsychDefaultSetup(2);
+
+% Create a random number generator
+rand('seed', sum(100 * clock));
+
+% Setting the screen number to the external secondary monitor if there is one
+% connected
+screenNumber = max(Screen('Screens'));
+
+% Define black, white and grey
+white = WhiteIndex(screenNumber);
+grey = white / .5; % making a lighter background (otherwise will play on grey, which makes contrast too difficult to discern
+black = BlackIndex(screenNumber);
+
+% Open the screen, defined as 1000 x 1000 matrix
+[window, windowRect] = PsychImaging('OpenWindow', 0, [255 255 255], [0 0 1000 1000], screenNumber, grey, [], 32, 2);
+%Here screen is opened just for changing the SyncTest preferencesk
+
+% Flip to clear
+Screen('Flip', window);
+
+% Query the frame duration
+ifi = Screen('GetFlipInterval', window);
+
+% Set text size to 40 
+Screen('TextSize', window, 40);
+
+% Query the maximum priority level
+topPriorityLevel = MaxPriority(window);
+
+% Get the center coordinate of the window
+[xCenter, yCenter] = RectCenter(windowRect);
+
+% Set the blend function for the screen
+Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
 
-%% Merging Content %%
+%----------------------------------------------------------------------
+%                       Timing Information
+%----------------------------------------------------------------------
 
-KbStrokeWait;%waits for key press to begin game
+% Stimulus interval time in seconds and frames
+isiTimeSecs = 1;
+isiTimeFrames = round(isiTimeSecs / ifi);
 
-run("RGB_v11.m") % the RGB Anamaloscope game will indicate which level of the Stroop Test individuals will do!
+
+% Number of frames to wait before next stimulus
+waitframes = 1;
 
 
-if RGBscore <= .25
-    run("strooplevel1.m")
-    run("strooplevel2.m")
-    run("strooplevel3.m")
-    run('strooplevel4.m')
-elseif RGBscore <=.5
-    run("strooplevel2.m")
-    run("strooplevel3.m")
-    run('strooplevel4.m')
-elseif RGBscore <=.75
-    run("strooplevel3.m")
-    run('strooplevel4.m')
-else 
-    run('strooplevel4.m')
+%----------------------------------------------------------------------
+%                       Keyboard Presses
+%----------------------------------------------------------------------
+
+% Here we are defining the keys that we need to use for responses.
+% We will be using escape to escape the task.
+upKey = KbName('up');
+downKey = KbName('downarrow');
+leftKey = KbName('leftarrow');
+rightKey = KbName('rightarrow');
+escapeKey= KbName('ESCAPE');
+
+DrawFormattedText(window, 'Choose which task you want to play! \n\n For RGB Anomaloscope, press the up key! \n\n For Priming Test, press the down key! \n\n For Stroop Test, press the left key!, \n\n For the Working Memory Test, press the right key! \n\n  You can quit anytime by pressing ESC!','center', 'center', black); %game menu
+Screen('Flip', window);
+KbStrokeWait
+
+[keyIsDown,secs, keyCode] = KbCheck;
+if keyCode(escapeKey)
+            ShowCursor;
+            sca;
+            return
+        elseif keyCode(upKey)
+            run("RGB_v11.m")
+        elseif keyCode(leftKey)
+            run("strooplevel1.m")
+            run("strooplevel2.m")
+            run("strooplevel3.m")
+            run('strooplevel4.m')
+  
 end
-
 run("Exitscreen.m")
 
